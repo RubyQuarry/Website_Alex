3e13e6ebc384b9c2e0852962325a2661e3fd0de1
A.J. Norton <ajn123@vt.edu>
2014-11-25 04:34:55 -0500

    added database to server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{root@104.131.13.122}
-role :web, %w{root@104.131.13.122}
-role :db,  %w{root@104.131.13.122}
+role :app, %w{root@104.131.13.122:80}
+role :web, %w{root@104.131.13.122:80}
+role :db,  %w{root@104.131.13.122:80}
 
 
 # Extended Server Syntax
