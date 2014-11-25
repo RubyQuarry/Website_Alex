32b68504bdf00fd61bc5c02ca98924c04bb4115a
A.J. Norton <ajn123@vt.edu>
2014-11-25 04:37:13 -0500

    added database to server

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,6 +43,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+
     end
   end
 
@@ -54,6 +55,7 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+      execute('rails s')
     end
   end
 
