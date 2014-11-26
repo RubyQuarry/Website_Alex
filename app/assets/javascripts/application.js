93fb00fba86eb9b98847083e10a982240b35c0e1
A.J. Norton <ajn123@vt.edu>
2014-11-26 06:36:52 -0500

    added working slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,7 +33,6 @@ $(function(){
 $(document).ready(function() {
 
   $("#owl-example").owlCarousel({
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
       items : 3,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
