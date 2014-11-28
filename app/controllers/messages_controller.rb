7df1a2f655d8e0ce0f224d83f48fa6c42db0d2f2
A.J. Norton <ajn123@vt.edu>
2014-11-28 04:13:46 -0500

    added message controller #create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
   def create
+    message = Message.new message_params
+    if message.save
+      
+    end
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+
+
+  def message_params
+    params.require(:message).permit(:name, :email, :content)
   end
 end
