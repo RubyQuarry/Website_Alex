f05b0ad13e16b34791bc225b417495e22d0e11fc
A.J. Norton <ajn123@vt.edu>
2014-12-07 03:47:26 -0500

    another scrollr try

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@ $(document).ready(function()
   var s = skrollr.init({
     render: function(data) {
       //Debugging - Log the current scroll position.
-      console.log(data.curTop);
+      //console.log(data.curTop);
     }
   });
 
