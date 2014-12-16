bce38e7198d63a3b71b67529e4247dd16ea127a7
A.J. Norton <ajn123@vt.edu>
2014-12-16 16:04:08 -0500

    updated unicorn deployment file

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,43 +1,49 @@
 
+
+# paths
+app_path = "/var/www/appleJuiceScholars"
+working_directory "#{app_path}/current"
+pid               "#{app_path}/current/tmp/pids/unicorn.pid"
+
+# listen
+listen "/tmp/unicorn-www.ajnorton.com.socket", :backlog => 64
+
+# logging
+stderr_path "log/unicorn.stderr.log"
+stdout_path "log/unicorn.stdout.log"
+
+# workers
 worker_processes 4
-user "root"
-working_directory "/var/www/appleJuiceScholars/current" # available in 0.94.0+
 
-listen "/tmp/.sock", :backlog => 64
-listen 80, :tcp_nopush => true
+# use correct Gemfile on restarts
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
+end
 
-timeout 30
-
-# feel free to point this anywhere accessible on the filesystem
-pid "/var/www/appleJuiceScholars/current/tmp/pids/unicorn.pid"
-
-stderr_path "/var/unicorn.stderr.log"
-stdout_path "/var/unicorn.stdout.log"
-
-# combine REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+# preload
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and
-  GC.copy_on_write_friendly = true
 
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
-  #defined?(ActiveRecord::Base) and
-  #  ActiveRecord::Base.connection.disconnect!
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
 
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
   old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
+  if File.exists?(old_pid) && server.pid != old_pid
     begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
+      Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
     end
   end
 end
 
 after_fork do |server, worker|
-  # the following is *required* for Rails + "preload_app true",
-  #defined?(ActiveRecord::Base) and
-  #  ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
 end
