9035440cb7bb0f92e029839b6bfe64f03b55d890
ajn123 <ajn123@vt.edu>
2014-12-30 23:12:59 -0500

    removed rvm hook

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,10 +61,4 @@ namespace :deploy do
 end
 
 
-namespace :rvm do
-    task :trust_rvmrc do
-          run "rvm rvmrc trust #{release_path}"
-            end
-end
 
-after "deploy", "rvm:trust_rvmrc"
