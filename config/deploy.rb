c36c49d115ac75e2176bbaf3fdfa72c3a7c21e17
ajn123 <ajn123@vt.edu>
2015-01-19 15:28:36 -0500

    tried to restart unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :stage, :production
 # set :pty, true
 
 # Default value for :linked_files is []
-  set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
   set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
